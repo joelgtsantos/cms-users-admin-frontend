492bf65af9a901d8689f9d2122c4fad37583e0b0
Joel Santos <joelgtsantos@gmail.com>
2018-02-08 16:19:51 -0600

    ADD basic configuration to login with social apps

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -15,21 +15,44 @@ angular
     'ngResource',
     'ngRoute',
     'ngSanitize',
-    'ngTouch'
+    'ngTouch',
+    'ui.router',
+    'socialLogin'
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
+  .config(function ($routeProvider, $urlRouterProvider, $stateProvider, socialProvider) {
+      var mainState = {
+        name: 'hello',
+        url: '/',
         templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl',
-        controllerAs: 'about'
-      })
-      .otherwise({
-        redirectTo: '/'
+        controller: 'MainCtrl'
+      }
+
+      var aboutState = {
+        name: 'about',
+        url: '/about',
+        template: '<h3>Its the UI-Router hello world app!</h3>'
+      }
+
+      $stateProvider.state(mainState);
+      $stateProvider.state(aboutState);
+      $urlRouterProvider.otherwise('/');
+
+      socialProvider.setGoogleKey("729418284493-jmftrbbe028p37pgmsfvjfpvlm0cnmt6.apps.googleusercontent.com");
+  })
+
+  // run blocks
+  .run(function($rootScope) {
+    // inject callback login instance 
+     $rootScope.$on('event:social-sign-in-success', function(event, userDetails){
+        console.log(userDetails);
       });
+
+
+     $rootScope.$on('event:social-sign-out-success', function(event, logoutStatus){
+        console.log("Logout2");
+        console.log(logoutStatus);
+     }); 
   });
+
+
+
---
+++ app/scripts/services/mainservice.js
@@ -0,0 +1,52 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name usersAdminApp.mainService
+ * @description
+ * # mainService
+ * Service in the usersAdminApp.
+ */
+angular.module('usersAdminApp')
+  .factory('mainService', function ($http, $q) {
+    // AngularJS will instantiate a singleton by calling "new" on this function
+    var ctxPath = 'http://localhost:8080/app/api/';//window.location.pathname.substring(0, window.location.pathname.indexOf("/",2));
+
+    function registerUser(user) {
+		var defer = $q.defer();
+		user.timezone= "";
+		user.preferredLanguages = "";
+
+	    $http({
+	    	method:"POST",
+            url: ctxPath + "user",
+            data: user,                
+            headers: {'Content-Type': 'application/json'}
+	    }).then(function(response) {
+	    	defer.resolve(response.data);
+	    });
+	    
+	    return defer.promise;
+	}
+
+	 function addUserParticipation(user) {
+		var defer = $q.defer();
+
+	    $http({
+	    	method:"POST",
+            url: ctxPath + "participation",
+            data: user,                
+            headers: {'Content-Type': 'application/json'}
+	    }).then(function(response) {
+	    	defer.resolve(response.data);
+	    });
+	    
+	    return defer.promise;
+	}
+
+	// expose a public API
+	return {
+		registerUser: registerUser,
+		addUserParticipation: addUserParticipation
+	};
+  });
